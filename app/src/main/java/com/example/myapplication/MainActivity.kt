043bc70581d305eb406340dfdd8a9eb647c8ec32
anreh <anurehka@gmail.com>
2022-07-20 11:39:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var student : Student
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        binding.btnSetValue.setOnClickListener(){
+            student = Student("Ali", 20, "RSD")
+            binding.myData = student
+        }
+
+        binding.btnDisplay.setOnClickListener(){
+            student.programme = "RIS"
+
+            binding.apply {
+                invalidateAll()
+            }
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Student.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+data class Student(
+    var name:String,
+    var age:Int,
+    var programme:String)
