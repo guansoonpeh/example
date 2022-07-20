b020380da2684a599fac2c8785be13b7fc25dbe3
anreh <anurehka@gmail.com>
2022-07-20 11:43:33 +0800

    change student age

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnDisplay.setOnClickListener(){
             student.programme = "RIS"
+            student.age = 21
 
             binding.apply {
                 invalidateAll()
